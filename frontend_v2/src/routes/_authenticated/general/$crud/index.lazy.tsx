ee40a3ea15c32ab9657d31f44150cb806e053bdc
edumes <66120423+edumes@users.noreply.github.com>
2025-02-03 19:10:37 +0000

    old frontend branch

--- frontend_v2/src/routes/_authenticated/general/$crud/index.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import Crud from '@/features/crud'
-import { createLazyFileRoute } from '@tanstack/react-router'
-
-export const Route = createLazyFileRoute('/_authenticated/general/$crud/')({
-  component: Crud,
-})
